31280ea8d9f71a838bb345b0b9955bd87a2983af
yzzlqyxc <yzzlqyxc@163.com>
2023-05-24 10:13:41 +0800

    20230524

--- net210/src/net/connection.rs
+++ net210/src/net/connection.rs
@@ -1,12 +1,18 @@
 use k210_hal::time::Bps;
-// use k210_hal::prelude::_k210_hal_serial_SerialExt;
+use k210_hal::prelude::_k210_hal_serial_SerialExt;
+use core::fmt::Write;
 
-
-pub fn connect() {
+pub fn sent() {
     let ph = k210_hal::Peripherals::take().unwrap();
     let clocks = k210_hal::clock::Clocks::new();
-    let pins = (ph.pins.pin13, ph.pins.pin14);
+    let pins_wifi = (ph.pins.pin13, ph.pins.pin14);
+    let pins_usb= (ph.pins.pin4, ph.pins.pin5);
 
-    let t = ph.UART1.configure(pins, Bps(115200), &clocks);
+    let wifi_s = ph.UART1.configure(pins_wifi, Bps(115200), &clocks);
+    let usb_s = ph.UART3.configure(pins_usb, Bps(115200), &clocks);
 
+    let (mut tx, rx) = usb_s.split();
+    let mut stdout = k210_hal::stdout::Stdout(&mut tx);
+    
+    writeln!(stdout, "hello");
 }
--- net210/src/net/mod.rs
+++ net210/src/net/mod.rs
@@ -1,8 +1,11 @@
-mod dvc;
-mod cfg;
-mod connection;
+// use smoltcp::storage;
 
-pub fn test() {
-    // connection::connect();    
+// mod dvc;
+// mod connection;
 
-}
+// pub fn sent() {
+//     let (rx_buf, tx_buf) = ([0u8;1024], [0u8; 1024]);
+//     let t = smoltcp::storage::RingBuffer::new(storage);
+//     let sok = smoltcp::socket::TcpSocket::new(tx_buf, rx_buf);
+
+// }
